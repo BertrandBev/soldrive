750aa320072e603e1df1d165223cb69cb7fb0135
BertrandBev <bertrand.bevillard@gmail.com>
2022-08-17 14:59:01 +0200

    fixed typo

--- src/api/chain-api.ts
+++ src/api/chain-api.ts
@@ -39,7 +39,7 @@ const programID = new PublicKey(address);
 
 // Re-export types
 export type Keyed<T> = solApi.Keyed<T>;
-export type File = solApi.FileFolder;
+export type File = solApi.File;
 export type Folder = solApi.Folder;
 export type User = solApi.User;
 export type Access = solApi.Access;
